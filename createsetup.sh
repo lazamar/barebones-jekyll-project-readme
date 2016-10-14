71f10622f7be28718e0402733022779330e6e194
lazamar <lazaronijunior@gmail.com>
2016-10-14 14:17:11 +0100

    x

--- createsetup.sh
+++ createsetup.sh
@@ -2,11 +2,15 @@ cat > .git/hooks/pre-push << EOF
 #!/bin/sh
 we_are_in_gh_pages="\$(git branch | grep -G '* gh-pages')"
 
-if [ ! we_are_in_gh-pages ]; then
-  git checkout gh-pages &&
-  git rebase master &&
-  git push -f &&
-  git checkout master # go back to master branch
+if [ ! we_are_in_gh-pages ];
+  then
+    echo "Workeddddddddddddd"
+    git checkout gh-pages &&
+    git rebase master &&
+    git push -f &&
+    git checkout master # go back to master branch
+  else
+    echo "didn't workkkkkkkkkkkkkkkkkkkkkkkkkkk"
 fi
 EOF
 

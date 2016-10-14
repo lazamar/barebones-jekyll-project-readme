eb3ad05edd7595dc467eb4e65f90fabb50dfb7ce
lazamar <lazaronijunior@gmail.com>
2016-10-14 13:44:34 +0100

    Remove createsetup file

--- createsetup.sh
+++
@@ -1,9 +0,0 @@
-cat > .git/hooks/pre-push << EOF
-#!/bin/sh
-git checkout gh-pages &&
-git rebase master &&
-git push -f &&
-git checkout master # go back to master branch
-EOF
-
-chmod 775 .git/hooks/pre-push
